d6383a1d3e34177f5c90867ccd1a2893d0592690
Malte Franken <exxamalte@users.noreply.github.com>
2022-02-17 12:25:23 +1100

    clean up of setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,28 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+NAME = "georss_nrcan_earthquakes_client"
+AUTHOR = "Malte Franken"
+AUTHOR_EMAIL = "coding@subspace.de"
+DESCRIPTION = (
+    "A GeoRSS client library for the Natural Resources Canada Earthquakes feed."
+)
+URL = "https://github.com/exxamalte/python-georss-nrcan-earthquakes-client"
+
 REQUIRES = [
     "georss_client>=0.14",
 ]
 
 setup(
-    name="georss_nrcan_earthquakes_client",
+    name=NAME,
     version="0.3",
-    author="Malte Franken",
-    author_email="coding@subspace.de",
-    description="A GeoRSS client library for the Natural Resources Canada Earthquakes feed.",
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
     license="Apache-2.0",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/exxamalte/python-georss-nrcan-earthquakes-client",
+    url=URL,
     packages=find_packages(exclude=("tests*",)),
     classifiers=[
         "Programming Language :: Python :: 3.7",
